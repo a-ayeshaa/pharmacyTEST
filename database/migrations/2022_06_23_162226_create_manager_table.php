f769b433986918c4419a35066c0e543e2d828d22
Tonmoy10 <tonmoysaha1225@gmail.com>
2022-07-05 00:23:34 +0600

    tim

--- database/migrations/2022_06_23_162226_create_manager_table.php
+++ database/migrations/2022_06_23_162226_create_manager_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('manager_email')->unique();
             //$table->date('manager_dob');
             $table->timestamps();
+            $table->string("image")->nullable();
         });
     }
 
